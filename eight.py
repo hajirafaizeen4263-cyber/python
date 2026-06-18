87f928f76ee28448f832ddb3051d2d07d663107b
Hajira Fizeen <hajirafaizeen4263@gmail.com>
2026-06-18 13:06:25 +0530

    Add files via upload

---
+++ eight.py
@@ -0,0 +1,63 @@
+#sequence
+#' '
+#" "
+#''' '''
+
+#sum of digits in a number
+
+
+# num=int(input("enter the digits:"))
+# sum=0
+# while num>0:
+#       num=num//10
+#       print(num)
+#       sum=sum+num
+# print(sum)
+
+
+#find the reverse of a number
+
+# num=12345
+# rev=0
+# while num>0:
+#      b= num %10
+#      rev=rev*10+b
+#      num=num//10
+# print(rev)
+
+
+#factorial of a number
+# num=int(input("enter the number:"))
+# fact=1
+# while num>0:
+#      fact=fact *num
+#      num=num-1
+# print(fact)
+
+#100 numbers sum
+# i=1
+# sum=0
+# for i in range(0,101):
+#      sum=sum+i
+# print(sum)
+
+#factorial of a number
+# fact=1
+# for i in range(1,5):
+#     fact=fact*i
+# print(fact)
+#multiplication table of a number
+
+# num=2
+# for i in range(1,11):
+#     print(f"{num} x {i} = {num*i}")
+
+#sum and average of a number
+# count=int(input("enter the count:"))
+# sum=0
+# for i in range(count):
+#     num=int(input("enter the num:"))
+#     sum=sum+num
+# print(f"sum is {sum}")
+# print(f"average={sum/count}")
+    
